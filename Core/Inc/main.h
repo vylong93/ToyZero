4b80c9fe7b423d465535d58138dacb29da0e542d
Long Vy. Dang <vylong93khtn@gmail.com>
2019-09-24 09:18:29 +0700

    Test three channel PWM and rainbow color
    Signed-off-by: Long Vy. Dang <vylong93khtn@gmail.com>

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -37,7 +37,11 @@ extern "C" {
 
 /* Exported types ------------------------------------------------------------*/
 /* USER CODE BEGIN ET */
-
+typedef enum led_rgb {
+  led0 = GPIO_PIN_13,
+  led1 = GPIO_PIN_14,
+  led2 = GPIO_PIN_15,
+} led_rgb_t;
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -45,7 +45,68 @@
 TIM_HandleTypeDef htim1;
 
 /* USER CODE BEGIN PV */
+const uint8_t lights[360]={
+  0,   0,   0,   0,   0,   1,   1,   2,
+  2,   3,   4,   5,   6,   7,   8,   9,
+ 11,  12,  13,  15,  17,  18,  20,  22,
+ 24,  26,  28,  30,  32,  35,  37,  39,
+ 42,  44,  47,  49,  52,  55,  58,  60,
+ 63,  66,  69,  72,  75,  78,  81,  85,
+ 88,  91,  94,  97, 101, 104, 107, 111,
+114, 117, 121, 124, 127, 131, 134, 137,
+141, 144, 147, 150, 154, 157, 160, 163,
+167, 170, 173, 176, 179, 182, 185, 188,
+191, 194, 197, 200, 202, 205, 208, 210,
+213, 215, 217, 220, 222, 224, 226, 229,
+231, 232, 234, 236, 238, 239, 241, 242,
+244, 245, 246, 248, 249, 250, 251, 251,
+252, 253, 253, 254, 254, 255, 255, 255,
+255, 255, 255, 255, 254, 254, 253, 253,
+252, 251, 251, 250, 249, 248, 246, 245,
+244, 242, 241, 239, 238, 236, 234, 232,
+231, 229, 226, 224, 222, 220, 217, 215,
+213, 210, 208, 205, 202, 200, 197, 194,
+191, 188, 185, 182, 179, 176, 173, 170,
+167, 163, 160, 157, 154, 150, 147, 144,
+141, 137, 134, 131, 127, 124, 121, 117,
+114, 111, 107, 104, 101,  97,  94,  91,
+ 88,  85,  81,  78,  75,  72,  69,  66,
+ 63,  60,  58,  55,  52,  49,  47,  44,
+ 42,  39,  37,  35,  32,  30,  28,  26,
+ 24,  22,  20,  18,  17,  15,  13,  12,
+ 11,   9,   8,   7,   6,   5,   4,   3,
+  2,   2,   1,   1,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0};
 
+const uint8_t HSVlights[61] =
+{0, 4, 8, 13, 17, 21, 25, 30, 34, 38, 42, 47, 51, 55, 59, 64, 68, 72, 76,
+81, 85, 89, 93, 98, 102, 106, 110, 115, 119, 123, 127, 132, 136, 140, 144,
+149, 153, 157, 161, 166, 170, 174, 178, 183, 187, 191, 195, 200, 204, 208,
+212, 217, 221, 225, 229, 234, 238, 242, 246, 251, 255};
+
+const uint8_t HSVpower[121] =
+{0, 2, 4, 6, 8, 11, 13, 15, 17, 19, 21, 23, 25, 28, 30, 32, 34, 36, 38, 40,
+42, 45, 47, 49, 51, 53, 55, 57, 59, 62, 64, 66, 68, 70, 72, 74, 76, 79, 81,
+83, 85, 87, 89, 91, 93, 96, 98, 100, 102, 104, 106, 108, 110, 113, 115, 117,
+119, 121, 123, 125, 127, 130, 132, 134, 136, 138, 140, 142, 144, 147, 149,
+151, 153, 155, 157, 159, 161, 164, 166, 168, 170, 172, 174, 176, 178, 181,
+183, 185, 187, 189, 191, 193, 195, 198, 200, 202, 204, 206, 208, 210, 212,
+215, 217, 219, 221, 223, 225, 227, 229, 232, 234, 236, 238, 240, 242, 244,
+246, 249, 251, 253, 255};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -58,21 +119,66 @@ static void MX_TIM1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void blinkHigh(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
+void blinkLedOnBoard()
 {
-    HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
     HAL_Delay(1);
-    HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_RESET);
-    HAL_Delay(499);
-}
-
-void blinkLow(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
-{
-    HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_RESET);
-    HAL_Delay(1);
-    HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
     HAL_Delay(999);
 }
+void setRGB(led_rgb_t led, uint8_t r, uint8_t g, uint8_t b) {
+  /* Deselect all LEDs */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_SET);
+
+  /* Set color */
+  htim1.Instance->CCR1 = r;
+  htim1.Instance->CCR2 = g;
+  htim1.Instance->CCR3 = b;
+
+  /* Active selected LED */
+  HAL_GPIO_WritePin(GPIOB, led, GPIO_PIN_RESET);
+}
+void fadeIn(led_rgb_t led, uint8_t r, uint8_t g, uint8_t b) {
+  for (int i = 0; i < 256; i++) {
+    setRGB(led, r, g, b);
+    HAL_Delay(5);
+  }
+}
+void fadeOut(led_rgb_t led, uint8_t r, uint8_t g, uint8_t b) {
+  for (int i = 255; i >= 0; i--) {
+    setRGB(led, r, g, b);
+    HAL_Delay(5);
+  }
+}
+
+/* Ref: https://www.instructables.com/id/How-to-Make-Proper-Rainbow-and-Random-Colors-With-/ */
+void trueHSV(led_rgb_t led, int angle) /* the real HSV rainbow */
+{
+  uint8_t red, green, blue;
+
+  if (angle<60) {red = 255; green = HSVlights[angle]; blue = 0;} else
+  if (angle<120) {red = HSVlights[120-angle]; green = 255; blue = 0;} else
+  if (angle<180) {red = 0, green = 255; blue = HSVlights[angle-120];} else
+  if (angle<240) {red = 0, green = HSVlights[240-angle]; blue = 255;} else
+  if (angle<300) {red = HSVlights[angle-240], green = 0; blue = 255;} else
+                 {red = 255, green = 0; blue = HSVlights[360-angle];}
+  setRGB(led, red, green, blue);
+}
+void powerHSV(led_rgb_t led, int angle) /* the 'power-conscious' HSV rainbow */
+{
+  uint8_t red, green, blue;
+  if (angle<120) {red = HSVpower[120-angle]; green = HSVpower[angle]; blue = 0;} else
+  if (angle<240) {red = 0;  green = HSVpower[240-angle]; blue = HSVpower[angle-120];} else
+                 {red = HSVpower[angle-240]; green = 0; blue = HSVpower[360-angle];}
+    setRGB(led, red, green, blue);
+}
+void sineLED(led_rgb_t led, int angle) /* sine wave rainbow */
+{
+  setRGB(led, lights[(angle+120)%360], lights[angle],  lights[(angle+240)%360]);
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -106,10 +212,20 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_SET);
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+  for (int i = 0; i < 256; i++) {
+    setRGB(led0 | led1 | led2, i, 0, i);
+    HAL_Delay(2);
+  }
+  for (int i = 255; i >= 0; i--) {
+    setRGB(led0 | led1 | led2, i, 0, i);
+    HAL_Delay(2);
+  }
+  for (int i = 0; i < 3; i ++) {
+    blinkLedOnBoard();
+  }
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -119,7 +235,18 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    blinkLow(GPIOC, GPIO_PIN_13);
+    for (int k = 0; k < 360; k++)
+    {
+      sineLED(led2, k);
+      HAL_Delay(30);
+      /*
+      for (int d = 0; d < 60; d++) {
+        sineLED(led2|led1|led0, k);
+        powerHSV(led2|led1|led0, k);
+        trueHSV(led2|led1|led0, k);
+      }
+      */
+    }
   }
   /* USER CODE END 3 */
 }
@@ -182,9 +309,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 32;
+  htim1.Init.Prescaler = 20-1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 62500;
+  htim1.Init.Period = 256-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -208,13 +335,23 @@ static void MX_TIM1_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 31250;
+  sConfigOC.Pulse = 1-1;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
   sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.Pulse = 1-1;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.Pulse = 1-1;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
   {
     Error_Handler();
   }
@@ -257,9 +394,6 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_SET);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9|GPIO_PIN_10, GPIO_PIN_RESET);
-
   /*Configure GPIO pin : PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
@@ -273,13 +407,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PA9 PA10 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
